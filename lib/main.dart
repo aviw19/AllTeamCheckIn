6852304e23f9f1370e2c432633dcb54a3554696a
aviw19 <rsavi0512@gmail.com>
2022-01-28 14:27:51 +0530

    basic google sign in functionality complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:allteamcheckin/Screens/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SignInPage(),
     );
   }
 }
